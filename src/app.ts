c966bc94fd993bb27f9623ac7c188debd1ef67c1
Josua Schmid <josua.schmid@renuo.ch>
2021-01-06 17:07:09 +0100

    Call Renuo Redmine

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@
  */
 
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import { Color3 } from '@microsoft/mixed-reality-extension-sdk';
+import * as https from 'https';
 
 /**
  * The main class of this app. All the logic goes here.
  */
-export default class HelloWorld {
+export default class RedmineSprint {
 	private text: MRE.Actor = null;
-	private cube: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 
 	constructor(private context: MRE.Context) {
@@ -32,124 +33,72 @@ export default class HelloWorld {
 					app: { position: { x: 0, y: 0.5, z: 0 } }
 				},
 				text: {
-					contents: "Hello Ren!",
+					contents: "Calling Redmine…",
 					anchor: MRE.TextAnchorLocation.MiddleCenter,
-					color: { r: 30 / 255, g: 206 / 255, b: 213 / 255 },
+					color: Color3.FromHexString('#27d79d'),
 					height: 0.3
-				}
-			}
-		});
-
-		// Here we create an animation for our text actor. First we create animation data, which can be used on any
-		// actor. We'll reference that actor with the placeholder "text".
-		const spinAnimData = this.assets.createAnimationData(
-			// The name is a unique identifier for this data. You can use it to find the data in the asset container,
-			// but it's merely descriptive in this sample.
-			"Spin",
-			{
-				// Animation data is defined by a list of animation "tracks": a particular property you want to change,
-				// and the values you want to change it to.
-				tracks: [{
-					// This animation targets the rotation of an actor named "text"
-					target: MRE.ActorPath("text").transform.local.rotation,
-					// And the rotation will be set to spin over 20 seconds
-					keyframes: this.generateSpinKeyframes(20, MRE.Vector3.Up()),
-					// And it will move smoothly from one frame to the next
-					easing: MRE.AnimationEaseCurves.Linear
-				}]
-			});
-		// Once the animation data is created, we can create a real animation from it.
-		spinAnimData.bind(
-			// We assign our text actor to the actor placeholder "text"
-			{ text: this.text },
-			// And set it to play immediately, and bounce back and forth from start to end
-			{ isPlaying: true, wrapMode: MRE.AnimationWrapMode.PingPong });
-
-		// Load a glTF model before we use it
-		const cubeData = await this.assets.loadGltf('altspace-cube.glb', "box");
-
-		// spawn a copy of the glTF model
-		this.cube = MRE.Actor.CreateFromPrefab(this.context, {
-			// using the data we loaded earlier
-			firstPrefabFrom: cubeData,
-			// Also apply the following generic actor properties.
-			actor: {
-				name: 'Altspace Cube',
-				// Parent the glTF model to the text actor, so the transform is relative to the text
-				parentId: this.text.id,
-				transform: {
-					local: {
-						position: { x: 0, y: -1, z: 0 },
-						scale: { x: 0.4, y: 0.4, z: 0.4 }
+				},
+				collider: {
+					geometry: {
+						shape: MRE.ColliderType.Sphere,
+						radius: 0.6
 					}
-				}
+				},
+				
 			}
 		});
 
-		// Create some animations on the cube.
-		const flipAnimData = this.assets.createAnimationData(
-			// the animation name
-			"DoAFlip",
-			{ tracks: [{
-				// applies to the rotation of an unknown actor we'll refer to as "target"
-				target: MRE.ActorPath("target").transform.local.rotation,
-				// do a spin around the X axis over the course of one second
-				keyframes: this.generateSpinKeyframes(1.0, MRE.Vector3.Right()),
-				// and do it smoothly
-				easing: MRE.AnimationEaseCurves.Linear
-			}]}
-		);
-		// apply the animation to our cube
-		const flipAnim = await flipAnimData.bind({ target: this.cube });
-
-		// Set up cursor interaction. We add the input behavior ButtonBehavior to the cube.
+		// Set up cursor interaction. We add the input behavior ButtonBehavior to the text.
 		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
-		const buttonBehavior = this.cube.setBehavior(MRE.ButtonBehavior);
+		const buttonBehavior = this.text.setBehavior(MRE.ButtonBehavior);
 
 		// Trigger the grow/shrink animations on hover.
 		buttonBehavior.onHover('enter', () => {
 			// use the convenience function "AnimateTo" instead of creating the animation data in advance
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.5, y: 0.5, z: 0.5 } } } },
+			MRE.Animation.AnimateTo(this.context, this.text, {
+				destination: { transform: {
+				  local: { scale: { x: 1.3, y: 1.3, z: 1.3 } }
+				} },
 				duration: 0.3,
 				easing: MRE.AnimationEaseCurves.EaseOutSine
 			});
 		});
 		buttonBehavior.onHover('exit', () => {
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
+			MRE.Animation.AnimateTo(this.context, this.text, {
+				destination: { transform: {
+					local: { scale: { x: 1, y: 1, z: 1 } }
+				} },
 				duration: 0.3,
 				easing: MRE.AnimationEaseCurves.EaseOutSine
 			});
 		});
 
-		// When clicked, do a 360 sideways.
-		buttonBehavior.onClick(_ => {
-			flipAnim.play();
-		});
-	}
+		const options: https.RequestOptions = {
+			hostname: 'redmine.renuo.ch',
+			port: 443,
+			path: '/issues.json?cf_8=!1&project.cf_6=!0&status_id=13',
+			headers: {
+				'X-Redmine-API-Key': process.env.REDMINE_API_KEY
+			}
+		};
 
-	/**
-	 * Generate keyframe data for a simple spin animation.
-	 * @param duration The length of time in seconds it takes to complete a full revolution.
-	 * @param axis The axis of rotation in local space.
-	 */
-	private generateSpinKeyframes(duration: number, axis: MRE.Vector3): Array<MRE.Keyframe<MRE.Quaternion>> {
-		return [{
-			time: 0 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 0)
-		}, {
-			time: 0.25 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI / 2)
-		}, {
-			time: 0.5 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, Math.PI)
-		}, {
-			time: 0.75 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 3 * Math.PI / 2)
-		}, {
-			time: 1 * duration,
-			value: MRE.Quaternion.RotationAxis(axis, 2 * Math.PI)
-		}];
+		https.get(options, (res) => {
+			let body = "";
+	
+			res.on("data", (chunk) => {
+					body += chunk;
+			});
+	
+			res.on("end", () => {
+					try {
+							let json = JSON.parse(body);
+							this.text.text.contents = `${json.total_count} tickets to estimate`;
+					} catch (error) {
+							console.error(error.message);
+					};
+			});
+		}).on("error", (error) => {
+				console.error(error.message);
+		});
 	}
 }
